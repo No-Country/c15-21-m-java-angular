25f7c1cf23b3359b0a9f101a53d59d64a4c712e7
Jhuliño Alexis Oroya Barreto <jhulino.oroya_consultor@mdp.com.pe>
2023-12-01 09:35:59 -0500

    feat: Agregar barra de navegación y pie de página

--- eco-tienda-frontend/tailwind.config.js
+++ eco-tienda-frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "et-turmeric": "#c1ca46",
+        "et-alabaster": " #fbfbfb",
+        "et-mule-fawn": " #95632e",
+        "et-primrose": " #f1e59f",
+        "et-citrine-white": " #f8f2cc",
+        "et-swamp-green": " #bcbb8c",
+        "et-mongoose": " #baa48c",
+        "et-wafer": " #ded4c4",
+        "et-westar": " #dcdcd0",
+      },
+    },
   },
   plugins: [],
 };
